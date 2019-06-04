f59059c1e9d1720be6c098667d6dbf02e18ff7fb
Charles Robertson <admin@cocohost.co>
2019-06-04 11:39:33 -0500

    Tweaked Glide settings

--- dist/js/init-glide.js
+++ dist/js/init-glide.js
@@ -1,1 +1,9 @@
-new Glide('.glide').mount();
+/* eslint-disable indent */
+// eslint-disable-next-line no-undef
+new Glide('.glide', {
+  type: 'carousel',
+  touchAngle: 30,
+  gap: 0,
+  touchRatio: 1
+
+}).mount();
--- src/js/init-glide.js
+++ src/js/init-glide.js
@@ -2,5 +2,8 @@
 // eslint-disable-next-line no-undef
 new Glide('.glide', {
   type: 'carousel',
-  touchAngle: 30
+  touchAngle: 30,
+  gap: 0,
+  touchRatio: 1
+
 }).mount();
