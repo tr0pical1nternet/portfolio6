81875e63e2223059d8aacbbb7b4b637294d2ea90
Charles Robertson <admin@cocohost.co>
2019-06-08 14:47:16 -0500

    Add remaining portfolio slides and init slider

--- src/js/init-glide.js
+++ src/js/init-glide.js
@@ -1,10 +1,18 @@
+/* eslint-disable no-undef */
 /* eslint-disable indent */
 // eslint-disable-next-line no-undef
 
-// new Glide('.glide', {
-//   type: 'carousel',
-//   touchAngle: 30,
-//   gap: 0,
-//   touchRatio: 1
+new Glide('.glide', {
+  type: 'carousel',
+  touchAngle: 30,
+  gap: 0,
+  touchRatio: 1
 
-// }).mount();
+}).mount();
+
+new Glide('.portfolio-info', {
+  type: 'slider',
+  touchAngle: 30,
+  gap: 0,
+  touchRatio: 1
+}).mount();
