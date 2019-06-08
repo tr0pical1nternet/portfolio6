02a38f01692eefb1cf0739e7da3e966da842c3c0
Charles Robertson <admin@cocohost.co>
2019-06-07 23:45:26 -0500

    Style bio image with shape-outside

--- src/js/init-glide.js
+++ src/js/init-glide.js
@@ -1,9 +1,10 @@
 /* eslint-disable indent */
 // eslint-disable-next-line no-undef
-new Glide('.glide', {
-  type: 'carousel',
-  touchAngle: 30,
-  gap: 0,
-  touchRatio: 1
 
-}).mount();
+// new Glide('.glide', {
+//   type: 'carousel',
+//   touchAngle: 30,
+//   gap: 0,
+//   touchRatio: 1
+
+// }).mount();
