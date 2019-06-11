7ea752030b8792e399ccb54d8877f1c581703794
Charles Robertson <admin@cocohost.co>
2019-06-11 11:23:22 -0500

    Integrated slick-slider on gallery carousel

--- dist/js/init-slick.js
+++ dist/js/init-slick.js
@@ -1,6 +1,12 @@
 /* eslint-disable no-undef */
 
 $(document).ready(function(){
+	$('.portfolio-gallery__slides').slick({
+		'arrows': false
+	});
+});
+
+$(document).ready(function(){
 	$('.portfolio-info__slides').slick({
 		'arrows': false
 	});
