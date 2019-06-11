971b5c5f02c5351edc316880bef2b9bec0c2ee46
Charles Robertson <admin@cocohost.co>
2019-06-10 22:09:20 -0500

    Trigger image swap from slider event

--- src/js/init-glide.js
+++ src/js/init-glide.js
@@ -2,7 +2,7 @@
 /* eslint-disable indent */
 // eslint-disable-next-line no-undef
 
-new Glide('.portfolio-gallery', {
+var portfolioGallerySlider = new Glide('.portfolio-gallery', {
   type: 'carousel',
   touchAngle: 30,
   gap: 0,
@@ -14,20 +14,33 @@ new Glide('.portfolio-gallery', {
     slide: 'portfolio-gallery__slide',
     arrows: 'portfolio-gallery__arrows',
     arrow: 'portfolio-gallery__arrow',
-    arrowNext: 'next',
-    arrowPrev: 'prev',
     bullets: 'portfolio-gallery__bullets',
     bullet: 'portfolio-gallery__bullet',
-    clone: 'clone',
-    active: 'active',
-    dragging: 'dragging',
-    disabled: 'disabled'
   }
-}).mount();
+});
 
-new Glide('.portfolio-info', {
+var portfolioInfoSlider = new Glide('.portfolio-info', {
   type: 'slider',
   touchAngle: 30,
   gap: 0,
   touchRatio: 1
 }).mount();
+
+var content = document.querySelector('.device__content--00');
+
+function swapContent() {
+  content.setAttribute('title', content.dataset.raventitle);
+  content.setAttribute('alt', content.dataset.ravenalt);
+  content.setAttribute('src', content.dataset.ravensrc);
+  content.setAttribute('srcset', content.dataset.ravensrcset);
+}
+
+portfolioGallerySlider.mount();
+
+portfolioInfoSlider.mount();
+
+// portfolioInfoSlider.on('move.after', function() {
+//   if (portfolioInfoSlider.index === 1) {
+//     swapContent();
+//   }
+// });
