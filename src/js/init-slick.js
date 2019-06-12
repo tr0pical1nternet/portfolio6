ab49f1d224154ff3d394dda94827267f6af12e0f
Charles Robertson <admin@cocohost.co>
2019-06-11 21:40:57 -0500

    Added a notes file to keep track of potential optimizations

--- src/js/init-slick.js
+++ src/js/init-slick.js
@@ -1,20 +1,26 @@
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
 
+// Initialize portfolio gallery slider
 $(document).ready(function(){
 	$('.portfolio-gallery__slides').slick({
 		'arrows': false
 		// 'variableWidth': true,
-		// 'centerMode': true
+		// 'centerMode': true,
+		// 'mobileFirst': true
 	});
 });
 
+// Initialize portfolio info slider
 $(document).ready(function(){
 	$('.portfolio-info__slides').slick({
 		'arrows': false
 	});
 });
 
+/// Handle image swapping and loading on portfolio item change
+
+// Select all website screenshots 
 var galleryImages = $('.portfolio-gallery__device-content');
 
 $('.portfolio-info__slides').on('beforeChange', function() {
@@ -22,8 +28,9 @@ $('.portfolio-info__slides').on('beforeChange', function() {
 
 	galleryImages.each(function(index) {
 		var img = $(this);
+		
+		// Each "device" is labled with a two digit index
 		var deviceNum = ('0' + index).slice(-2);
-		// console.log(deviceNum);
 
 		img.attr('title', img.attr('data-title-' + deviceNum));
 		img.attr('alt', img.attr('data-alt-' + deviceNum));
